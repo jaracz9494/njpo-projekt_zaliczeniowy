7852961c8ed780e5f248d4939869da8870e9bd87
jaracz9494 <jaracz9494@wp.pl>
2017-01-16 18:07:50 +0100

    Add files via upload

--- Plansza.java
+++ Plansza.java
@@ -1,5 +1,11 @@
 package projekt_zaliczeniowy;
 
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,20 +15,18 @@ package projekt_zaliczeniowy;
  *
  * @author Dominik
  */
+
+
 public class Plansza extends javax.swing.JFrame {
+    public int wartosc;
     
     public int typ() {
-        int wartosc=0;
         
-        if (Masztowiec1.isSelected()) {
-            wartosc=1;
-        }
-        
-        if (Masztowiec2.isSelected()) {
-            wartosc=2;
-        }
         return wartosc;
     }
+    
+    
+    
 
     /**
      * Creates new form Plansza
@@ -57,6 +61,7 @@ public class Plansza extends javax.swing.JFrame {
         Maszt3 = new javax.swing.JLabel();
         Maszt4 = new javax.swing.JLabel();
         test = new javax.swing.JTextField();
+        test1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -195,7 +200,9 @@ public class Plansza extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(test, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(203, 203, 203))
+                .addGap(36, 36, 36)
+                .addComponent(test1, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(100, 100, 100))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -220,7 +227,9 @@ public class Plansza extends javax.swing.JFrame {
                     .addComponent(Masztowiec4)
                     .addComponent(Maszt4, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(27, 27, 27)
-                .addComponent(test, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(test)
+                    .addComponent(test1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
 
@@ -229,23 +238,33 @@ public class Plansza extends javax.swing.JFrame {
 
     private void Masztowiec1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Masztowiec1ActionPerformed
         Maszt1.setText("zmiana");
+        wartosc=1;
     }//GEN-LAST:event_Masztowiec1ActionPerformed
 
     private void Masztowiec2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Masztowiec2ActionPerformed
         Maszt2.setText("zmiana");
+        wartosc=2;
     }//GEN-LAST:event_Masztowiec2ActionPerformed
 
     private void Masztowiec3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Masztowiec3ActionPerformed
         Maszt3.setText("zmiana");
+        wartosc=3;
     }//GEN-LAST:event_Masztowiec3ActionPerformed
 
     private void Masztowiec4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Masztowiec4ActionPerformed
         Maszt4.setText("zmiana");
+        wartosc=4;
     }//GEN-LAST:event_Masztowiec4ActionPerformed
 
     private void MapaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_MapaMouseClicked
-        Mapa.setValueAt(typ(), Mapa.getSelectedRow(), Mapa.getSelectedColumn());
-        test.setText(Integer.toString(Mapa.getSelectedRow()));
+        if (wartosc==0) {           
+        } else {
+            Mapa.setValueAt(wartosc, Mapa.getSelectedRow(), Mapa.getSelectedColumn());
+            Mapa.setSelectionBackground(Color.red);
+            //Mapa.getColumnModel().getColumn(4).setCellRenderer(new StatusColumnCellRenderer());
+        }
+        //test.setText(Integer.toString(Mapa.getSelectedRow()));
+        test.setText(Mapa.getCellRenderer(2, 2).toString());
     }//GEN-LAST:event_MapaMouseClicked
 
     private void testActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_testActionPerformed
@@ -301,5 +320,7 @@ public class Plansza extends javax.swing.JFrame {
     private javax.swing.ButtonGroup grupa;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField test;
+    private javax.swing.JLabel test1;
     // End of variables declaration//GEN-END:variables
+public javax.swing.JLabel test2;
 }
